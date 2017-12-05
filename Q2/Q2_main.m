78ddf2add37fd7e70402432f8396c5e2c4d90db8
Adhikary <sadhikary6@lwcimac07.tss.gatech.edu>
2017-12-05 16:47:50 -0500

    Q2 complete

--- Q2/Q2_main.m
+++ Q2/Q2_main.m
@@ -1,5 +1,6 @@
 %Load data
 load Q2.mat;
+% X = X(1:20,:);
 
 %Set parameters
 K=4; %You should do K=2, 3, 4
@@ -8,16 +9,17 @@ max_iter = 100;
 
 %Do clustering
 [C, I, Loss] = myKmeans(X, K, max_iter);
-%%
-% %Plot your result
-% figure
-% scatter(X(:,1),X(:,2),[],I);
-% hold on
-% plot(C(:,1),C(:,2),'xk','LineWidth',5,'MarkerSize',20);
-% title(strcat('Scatter of K=',int2str(K)))
-% set(gca,'FontSize',20)
-% 
-% figure
-% plot(Loss,'LineWidth',2);
-% title(strcat('Plot of K=',int2str(K)))
-% set(gca,'FontSize',20)
+
+
+% Plot your result
+figure
+scatter(X(:,1),X(:,2),[],I);
+hold on
+plot(C(:,1),C(:,2),'xk','LineWidth',5,'MarkerSize',20);
+title(strcat('Scatter of K=',int2str(K)))
+set(gca,'FontSize',20)
+
+figure
+plot(Loss,'LineWidth',2);
+title(strcat('Plot of K=',int2str(K)))
+set(gca,'FontSize',20)
--- Q2/clusterSumSquare.m
+++ Q2/clusterSumSquare.m
@@ -1,3 +1,12 @@
-function [sum] = clusterSumSquare(X,K,mu)
-
+%calculate the sum of within cluster sum of square distances to the centers
+function [squareSum] = clusterSumSquare(X,C,I,K,N)
+    squareSum = 0;
+    for k = 1:K
+        for i = 1:N
+            if I(i) == k %if the point is in the cluster
+                mu = C(I(i),:); %Pick the mean
+                squareSum = squareSum + norm(mu - X(i,:))^2; %add to squareSum
+            end
+        end
+    end
 end
