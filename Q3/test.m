4455b457c55567feb824798ffa6da17455da8e1d
Adhikary <sadhikary6@lwcimac07.tss.gatech.edu>
2017-12-05 18:58:07 -0500

    completed myROC

---
+++ Q3/test.m
@@ -0,0 +1,10 @@
+load fisheriris
+
+pred = meas(51:end,1:2);
+resp = (1:100)'>50;  % Versicolor = 0, virginica = 1
+
+mdl = fitglm(pred,resp,'Distribution','binomial','Link','logit');
+scores = mdl.Fitted.Probability;
+
+[X,Y,T,AUC] = perfcurve(species(51:end,:),scores,'virginica');
+
