d87d995899c290000d9bd93f0078931a36fe2278
Adhikary <sadhikary6@lwcimac07.tss.gatech.edu>
2017-12-05 19:58:07 -0500

    Q1 part a done

--- Q1/Q1_main.m
+++ Q1/Q1_main.m
@@ -7,53 +7,73 @@ load Q1.mat
 %%%%%%%%%%%%%%%         Your Code Starts Here         %%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%Do SVD of S, and pick the first column
 
+% %Possibly only works with Positive Semi Definite matrices
+% [U_real,S_real,V_real_svd] = svd(S);
+% v_real_svd = U(:,1);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%         Your Code Ends Here         %%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-disp('The ground truth PC is')
-disp(v_real)
+%get all eigenvalues and eigenvectors
+[V_s,D_s] = eig(S);
 
-%% Implement part b
-% with data X, you can solve 
-%the empirical first principle componenet v_est
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%         Your Code Starts Here         %%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%find the column number of the max eigenvalue
+[val maxCol_s] = max(max(D));
 
-
+%pick the eigenvector corresponding to the max eigenvalue
+v_real = V_s(:,maxCol_s);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%         Your Code Ends Here         %%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% disp('The ground truth PC is')
+% disp(v_real)
+
+% %% Implement part b
+% % with data X, you can solve 
+% %the empirical first principle componenet v_est
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %%%%%%%%%%%%%%%         Your Code Starts Here         %%%%%%%%%%%%%%%%%%%%%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%get all eigenvalues and eigenvectors
+% [V_x,D_x] = eig(X);
+% 
+% %find the column number of the max eigenvalue
+% [val maxCol_x] = max(max(D_x));
+% 
+% %pick the eigenvector corresponding to the max eigenvalue
+% v_est = V_x(:,maxCol_x);
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %%%%%%%%%%%%%%%         Your Code Ends Here         %%%%%%%%%%%%%%%%%%%%%
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 disp('The empirical PC is')
 disp(v_est)
-%% Power Iteration
-% you should implement this part in power_method.m
-batchsize = length(X);
-pass = 20;
-[v1, lambda1,loss_real1, loss_est1] = power_method(X, batchsize, pass ,v_real, v_est);
-myplot('Q1 c: Optimization error',loss_real1)
-myplot('Q1 c: Estimation error',loss_est1)
-
-%% Oja Iteration
-% you should implement this part in oja_method.m
-batchsize = 2;
-pass = 1;
-eta = 0.1;
-[v2, lambda2,loss_real2, loss_est2] = oja_method(X, eta, batchsize, pass ,v_real, v_est);
-myplot('Q1 d: Optimization error',loss_real2)
-myplot('Q1 d: Estimation error',loss_est2)
-
-
-function myplot(mytitle,y,x)
-    figure
-    if (nargin == 2)
-        plot(y,'LineWidth',2,'MarkerSize',20);
-    else
-        plot(x,y,'LineWidth',2,'MarkerSize',20);
-    end
-    title(mytitle)
-    set(gca,'FontSize',20)
-end
+% %% Power Iteration
+% % you should implement this part in power_method.m
+% batchsize = length(X);
+% pass = 20;
+% [v1, lambda1,loss_real1, loss_est1] = power_method(X, batchsize, pass ,v_real, v_est);
+% myplot('Q1 c: Optimization error',loss_real1)
+% myplot('Q1 c: Estimation error',loss_est1)
+% 
+% %% Oja Iteration
+% % you should implement this part in oja_method.m
+% batchsize = 2;
+% pass = 1;
+% eta = 0.1;
+% [v2, lambda2,loss_real2, loss_est2] = oja_method(X, eta, batchsize, pass ,v_real, v_est);
+% myplot('Q1 d: Optimization error',loss_real2)
+% myplot('Q1 d: Estimation error',loss_est2)
+% 
+% 
+% function myplot(mytitle,y,x)
+%     figure
+%     if (nargin == 2)
+%         plot(y,'LineWidth',2,'MarkerSize',20);
+%     else
+%         plot(x,y,'LineWidth',2,'MarkerSize',20);
+%     end
+%     title(mytitle)
+%     set(gca,'FontSize',20)
+% end
